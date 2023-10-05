0b82eda1b1d660122924288953eec23cbd294a75
yzu1122050 <147010624+yzu1122050@users.noreply.github.com>
2023-10-05 09:56:58 +0800

    Update main.cpp
    class word

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-/***    
- *   hw 20231005
- *   @author: 1122099
- *   @name: Wen-Pinn Fang
+/**   
+ *   homework 20231005
+ *   @author: 1122050
+ *   @name: 周育賢
 ***/
 #include <iostream>
 using namespace std;
